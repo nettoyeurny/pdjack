b355fdf03268f716902cde26c01f03198ba03132
Peter Brinkmann <peter.brinkmann@gmail.com>
2011-04-03 21:07:37 -0400

    now with proper instance management

--- src/org/puredata/jack/PdJackProcessor.java
+++ src/org/puredata/jack/PdJackProcessor.java
@@ -9,16 +9,29 @@ import org.puredata.core.PdBase;
 import com.noisepages.nettoyeur.jack.JackException;
 import com.noisepages.nettoyeur.jack.JackNativeClient;
 
+/**
+ * A simple class that ties Pure Data and Jack together. (Not thread-safe!)
+ * 
+ * @author Peter Brinkmann (peter.brinkmann@gmail.com)
+ */
 public class PdJackProcessor {
 
 	private static final int PD_BLOCK_SIZE = PdBase.blockSize();
+	private static PdJackProcessor instance = null;
 	
 	private final int nIn;
 	private final int nOut;
 	private final float[] inBuf;
 	private final float[] outBuf;
+	private JackNativeClient nativeClient = null;
 	
-	public PdJackProcessor(int nIn, int nOut) throws JackException {
+	/**
+	 * Initializes Pd and prepares input and output buffers for Jack.
+	 * 
+	 * @param nIn number of input channels
+	 * @param nOut number of output channels
+	 */
+	private PdJackProcessor(int nIn, int nOut) throws JackException {
 		if ((nIn < 0 || nOut < 0) || (nIn == 0 && nOut == 0)) {
 			throw new IllegalArgumentException("bad number of input/output channels");
 		}
@@ -30,12 +43,34 @@ public class PdJackProcessor {
 		PdBase.computeAudio(true);
 	}
 	
+	/**
+	 * This method makes sure that there's only one instance of this class at any given time;
+	 * an unfortunate limitation, but it's necessary as long as Pd only admits one global instance.
+	 * 
+	 * @param nIn number of input channels
+	 * @param nOut number of output channels
+	 */
+	public static PdJackProcessor createPdJackProcessor(int nIn, int nOut) throws JackException {
+		if (instance != null) {
+			instance.stop();
+		}
+		instance = new PdJackProcessor(nIn, nOut);
+		return instance;
+	}
+	
+	/**
+	 * Creates a new Jack client for this processor.  Since it only makes sense to have one
+	 * Jack client for Pd at any given time, this method will close the current client if
+	 * necessary.
+	 * 
+	 * @param name
+	 */
 	public JackNativeClient createClient(String name) throws JackException {
-		return new JackNativeClient(name, nIn, nOut) {
+		stop();
+		nativeClient = new JackNativeClient(name, nIn, nOut) {
 			@Override
 			protected void process(FloatBuffer[] inputs, FloatBuffer[] outputs) {
 				int jackBlockSize = (nIn > 0) ? inputs[0].capacity() : outputs[0].capacity();
-				assert jackBlockSize % PD_BLOCK_SIZE == 0;
 				for (int n = 0; n < jackBlockSize; n += PD_BLOCK_SIZE) {
 					for (int i = 0; i < nIn; i++) {
 						inputs[i].get(inBuf, i*PD_BLOCK_SIZE, PD_BLOCK_SIZE);
@@ -47,16 +82,24 @@ public class PdJackProcessor {
 				}
 			}
 		};
+		return nativeClient;
 	}
 	
+	private void stop(){ 
+		if (nativeClient != null) {
+			nativeClient.close();
+			nativeClient = null;
+		}
+	}
+	
+	// Simple main routine, mostly to check whether everything loads and starts correctly.
 	public static void main(String[] args) throws JackException, InterruptedException, IOException {
-		PdJackProcessor proc = new PdJackProcessor(1, 2);
+		PdJackProcessor proc = createPdJackProcessor(1, 2);
 		int patchId = PdBase.openPatch(new File("data/test.pd"));
 		JackNativeClient jackClient = proc.createClient("pd-client");
 		jackClient.connectInputPorts("system");
 		jackClient.connectOutputPorts("system");
-		while (true) {
-			Thread.sleep(100);
-		}
+		Thread.sleep(1000 * 3600);
+		PdBase.closePatch(patchId);
 	}
 }
