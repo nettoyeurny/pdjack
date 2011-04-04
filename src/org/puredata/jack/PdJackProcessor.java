8c4f02c00e48764a7b08e0029bc7b87c5cea2d7d
Peter Brinkmann <peter.brinkmann@gmail.com>
2011-04-03 21:19:39 -0400

    now with license

--- src/org/puredata/jack/PdJackProcessor.java
+++ src/org/puredata/jack/PdJackProcessor.java
@@ -1,3 +1,10 @@
+/**
+ * 
+ * For information on usage and redistribution, and for a DISCLAIMER OF ALL
+ * WARRANTIES, see the file, "LICENSE.txt," in this distribution.
+ * 
+ */
+
 package org.puredata.jack;
 
 import java.io.File;
